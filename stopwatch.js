8d01f6ea96833ec32cb57929afac6cab0f87f58b
Jerome Spooner <jspooner@brooklynsteamcenter.org>
2023-05-01 14:53:09 -0400

    added my stop reset and start

---
+++ stopwatch.js
@@ -0,0 +1,35 @@
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+let digits = document.getElementById('digits')
+
+let interval;
+function start() {
+    reset()
+alert("You clicked the start button")
+interval = setInterval(function () {
+   milliseconds = milliseconds + 10
+    
+    if (milliseconds >= 1000) {
+    seconds = seconds + 1
+    milliseconds = 0
+    }
+    if (seconds >= 60) {
+        minutes = minutes + 1
+        seconds = 0
+    }
+    digits.innerHTML = minutes +":" +String (seconds).padStart(2,'0') + ":" + milliseconds/10
+}, 10)
+   
+}
+function stop() {
+    clearInterval(interval)
+}
+function reset() {
+    stop()
+    milliseconds = 0
+    seconds = 0
+    minutes = 0
+    digits.innerHTML = "00:00:00"
+}
