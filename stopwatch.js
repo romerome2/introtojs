1b992212955fab6689c852be332e29286788e06d
Jerome Spooner <jspooner@brooklynsteamcenter.org>
2023-05-04 14:50:43 -0400

    Allot of stuff has been added

--- stopwatch.js
+++ stopwatch.js
@@ -3,15 +3,19 @@ let seconds = 0
 let minutes = 0
  
 let startButton = document.getElementById('startbutton')
-
+let lapButton = document.getElementById("lbut")
 
 let digits = document.getElementById('digits')
 let lapList = document.getElementById('Lap')
 
+lapButton.disabled = true
+
+
 let interval;
 function start() {
     startButton.disabled = true
-    
+    lapButton.disabled = false
+
 
 interval = setInterval(function () {
    milliseconds = milliseconds + 10
@@ -29,19 +33,24 @@ interval = setInterval(function () {
    
 }
 function stop() {
+
+    lapButton.disabled = true
     startButton.disabled = false
     clearInterval(interval)
 }
 function reset() {
+    lapButton.disabled = true
+
     stop()
     milliseconds = 0
     seconds = 0
     minutes = 0
     digits.innerHTML = "00:00:00"
-}
+    lapList.innerHTML = ""
 
+}
 function Lap() {
     let newListItem = document.createElement('li')
-    newListItem.textContent = ""
+    newListItem.textContent = minutes +":" +String (seconds).padStart(2,'0') + ":" + milliseconds/10
     lapList.appendChild(newListItem)
 }
